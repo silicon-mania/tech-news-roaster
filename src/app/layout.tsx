515b5ef06147e1b1039dfecdc4016d6b1961d2f5
Hugo Bayoud <hugo@siliconmania.tv>
2026-06-19 09:50:15 +0200

    Add meticulous CDN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,16 @@ export default function RootLayout({
   return (
     <html lang="en" className={`dark ${poppins.variable}`}>
       <body>
+        <head>
+          {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+            // eslint-disable-next-line @next/next/no-sync-scripts
+            <script
+              data-recording-token="oxdbsvyDG6TTTYt8E82inbMbUntcUwu8E9uCk6Pd"
+              data-is-production-environment="false"
+              src="https://snippet.meticulous.ai/v1/meticulous.js"
+            />
+          )}
+        </head>
         <TooltipProvider>{children}</TooltipProvider>
         <Toaster />
       </body>
